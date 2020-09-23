4815369683c907979c3cfae65589ccd13cf763b8
Ben McCurdy <bpmccurdy@gmail.com>
2020-09-22 22:30:28 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/**
+ * makes the carousel show the edges of the previous and next item
+ */
+ 
+$('.carousel-item', '.show-neighbors').each(function(){
+  var next = $(this).next();
+  if (! next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+}).each(function(){
+  var prev = $(this).prev();
+  if (! prev.length) {
+    prev = $(this).siblings(':last');
+  }
+  prev.children(':nth-last-child(2)').clone().prependTo($(this));
+});
+		
