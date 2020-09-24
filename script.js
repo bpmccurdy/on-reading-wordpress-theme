c66e3a14a9957702de23af95acda5b8c4e1f8609
Ben McCurdy <bpmccurdy@gmail.com>
2020-09-23 18:07:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,30 @@ $('.carousel-item', '.show-neighbors').each(function(){
   prev.children(':nth-last-child(2)').clone().prependTo($(this));
 });
 }(jQuery));
-		
+
+/**
+ * makes the combined navbar show up when scrolled down the page
+ */ 
+
+(function ($) {
+  $(document).ready(function(){
+    
+	// hide .navbar first
+	$(".navbar-combined").hide();
+	
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 100) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+
+	
+	});
+
+  });
+}(jQuery));
