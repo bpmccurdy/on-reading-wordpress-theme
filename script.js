734ce17283037fea188063eef736f1433fe7c168
Ben McCurdy <bpmccurdy@gmail.com>
2020-09-23 20:29:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/**
- * makes the carousel show the edges of the previous and next item
- */
-(function ($) {
-$('.carousel-item', '.show-neighbors').each(function(){
-  var next = $(this).next();
-  if (! next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-}).each(function(){
-  var prev = $(this).prev();
-  if (! prev.length) {
-    prev = $(this).siblings(':last');
-  }
-  prev.children(':nth-last-child(2)').clone().prependTo($(this));
-});
-}(jQuery));
 
 /**
  * makes the combined navbar show up when scrolled down the page
